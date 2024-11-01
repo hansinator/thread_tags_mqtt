ac835d6eec67cbab7193698ecb2d35d177840037
Hans-Gert Dahmen <hans-gert.dahmen@immu.ne>
2024-11-01 23:03:02 +0100

    clean up; support mqtt authentication

--- mqtt.py
+++ mqtt.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import datetime
 import logging as log
 import cfg
-from time import sleep,time
+from time import sleep, time
 import json
 import socket
 
@@ -11,34 +11,31 @@ conf = {}
 # -------------------- mqtt events -------------------- 
 def on_connect(lclient, userdata, flags, rc):
     global conf
-    log.info("mqtt> connected with result code "+str(rc))
-    if(conf["mqtt"]["subscribe"]):
+    log.info("mqtt> connected with result code " + str(rc))
+    if conf["mqtt"]["subscribe"]:
         for sub in userdata["mqtt"]["subscriptions"]:
-            log.info("mqtt> Subscription to %s",sub)
+            log.info("mqtt> Subscription to %s", sub)
             lclient.subscribe(sub)
     else:
         log.info("mqtt> Subscriptions not enabled")
-    if(conf["mqtt"]["publish"]):
+    if conf["mqtt"]["publish"]:
         log.info("mqtt> Publishing enabled")
     else:
         log.info("mqtt> Publishing not enabled")
-            
-    
 
 def ruler_loop_forever():
-    while(True):
+    while True:
         sleep(10)
     return
 
-
-def mqtt_start(config,mqtt_on_message,start_looping):
+def mqtt_start(config, mqtt_on_message, start_looping):
     def mqtt_connect_retries(client):
         connected = False
-        while(not connected):
+        while not connected:
             try:
                 client.connect(config["mqtt"]["host"], config["mqtt"]["port"], config["mqtt"]["keepalive"])
                 connected = True
-                log.info(  "mqtt> connected to "+config["mqtt"]["host"]+":"+str(config["mqtt"]["port"])+" with id: "+ cid )
+                log.info("mqtt> connected to " + config["mqtt"]["host"] + ":" + str(config["mqtt"]["port"]) + " with id: " + cid)
             except socket.error:
                 log.error("socket.error will try a reconnection in 10 s")
                 sleep(10)
@@ -47,15 +44,19 @@ def mqtt_start(config,mqtt_on_message,start_looping):
     global conf
     conf = config
     clientMQTT = None
-    if(config["mqtt"]["publish"] or config["mqtt"]["subscribe"]):
+    if config["mqtt"]["publish"] or config["mqtt"]["subscribe"]:
         config["mqtt"]["enable"] = True
-        cid = config["mqtt"]["client_id"] +"_"+socket.gethostname()
-        clientMQTT = mqtt.Client(client_id=cid,userdata=config)
+        cid = config["mqtt"]["client_id"] + "_" + socket.gethostname()
+        clientMQTT = mqtt.Client(client_id=cid, userdata=config)
+        
+        # Setting the username and password for authentication
+        if "username" in config["mqtt"] and "password" in config["mqtt"]:
+            clientMQTT.username_pw_set(config["mqtt"]["username"], config["mqtt"]["password"])
+
         clientMQTT.on_connect = on_connect
         clientMQTT.on_message = mqtt_on_message
         mqtt_connect_retries(clientMQTT)
-        if(start_looping):
-            #the loop could be called in the run main loop()
+        if start_looping:
             clientMQTT.loop_start()
     else:
        config["mqtt"]["enable"] = False
